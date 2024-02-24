b0f19191da08b26cd5b1953d7700fb6d777d2781
Sumidsrfdftrd <physicistsumit@gmail.com>
2024-02-24 13:19:04 +0530

    main project

--- Neha/passwords/script.js
+++ Neha/passwords/script.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const timerContainers = document.querySelectorAll('.timer-container');
     timerContainers.forEach((timerContainer, index) => {
         const targetTime = new Date();
-        targetTime.setHours(23, 28, 0, 0);
+        targetTime.setHours(24, 13, 0, 0);
         let remainingTime = targetTime - new Date();
         const countdownId = `countdown${index + 1}`;
         updateCountdown(countdownId, remainingTime);
