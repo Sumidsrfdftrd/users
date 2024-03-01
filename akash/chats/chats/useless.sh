ad5d0b1e42fdb3486ad55470c204daff4388692d
Sumidsrfdftrd <physicistsumit@gmail.com>
2024-03-01 17:28:54 +0530

    main project

--- akash/chats/chats/useless.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#for ((i=51;i<55;i++))
-#do
-#     sed -i "s/id=\"password\" class=\"password-input\"/id=\"password\"/"  "friend${i}.html"
-#     sed -i "s/name=\"username\"/name=\"username\" class=\"password-input\"/"  "friend${i}.html"
-#     sed -i "s/enteredPassword === correctPassword/enteredPassword !== correctPassword/"  "friend${i}.html"
-#     sed -i "s/window.location.href = 'path\/to\/next\/page.html';/document.getElementById('error-message').innerText = 'Incorrect username or password!'; document.getElementById('error-message').style.color = 'red';/"  "friend${i}.html"
-#done
-sed -i "s/your\$##^@&*\$^_password/@username/" took.html
